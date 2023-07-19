29baeea9656085938461fad83efef97c2ff40f2b
josuezelaya <josuezelaya2727@gmail.com>
2023-07-18 18:28:00 -0600

    se modifico el programa con las inst del proyect 2

--- contactos.js
+++ contactos.js
@@ -1,40 +1,75 @@
 // Crear una lista de contactos con datos predefinidos
-let listaContactos = ["Juan Perez", "Maria Lopez", "Pedro Ramirez"];
+let listaContactos = [
+    {
+      id: 1,
+      nombres: "Juan",
+      apellidos: "Perez",
+      telefono: "123456789",
+      ubicaciones: {
+        ciudad: "Los Angeles",
+        direccion: "Dirección 1"
+      }
+    },
+    {
+      id: 2,
+      nombres: "Maria",
+      apellidos: "Lopez",
+      telefono: "987654321",
+      ubicaciones: {
+        ciudad: "La Ceiba",
+        direccion: "Dirección 2"
+      }
+    },
+    {
+      id: 3,
+      nombres: "Pedro",
+      apellidos: "Ramirez",
+      telefono: "456789123",
+      ubicaciones: {
+        ciudad: "San Pedro Sula",
+        direccion: "Dirección 3"
+      }
+    }
+  ];
 
 // Función para añadir un nuevo contacto a la lista
-function agregarContacto(nombreCompleto) {
-  listaContactos.push(nombreCompleto);
+function agregarContacto(contacto) {
 }
+listaContactos.push(contacto);
 
 // Función para borrar un contacto existente de la lista
-function borrarContacto(nombreCompleto) {
-  let indice = listaContactos.indexOf(nombreCompleto);
-  if (indice !== -1) {
-    listaContactos.splice(indice, 1);
+function borrarContacto(id) {
+    let indice = listaContactos.findIndex(function(contacto) {return contacto.id === id; });
+    if (indice !== -1) {
+      listaContactos.splice(indice, 1);
+    }
   }
-}
 
 // Función para imprimir los contactos en la consola
 function imprimirContactos() {
-  console.log("Lista de contactos:");
-  for (let i = 0; i < listaContactos.length; i++) {
-    console.log(listaContactos[i]);
+    console.log("Lista de contactos:");
+    for (let i = 0; i < listaContactos.length; i++) {
+      let contacto = listaContactos[i];
+      console.log(
+        "ID:", contacto.id,
+        "Nombre:", contacto.nombres,
+        "Apellido:", contacto.apellidos,
+        "Teléfono:", contacto.telefono,
+        "Ciudad:", contacto.ubicaciones.ciudad,
+        "Dirección:", contacto.ubicaciones.direccion
+      );
+    }
   }
-}
-
-console.log("Contactos iniciales:");
-imprimirContactos();
-
-console.log("\nAgregando nuevo contacto...");
-agregarContacto("Ana Gomez");
-
-console.log("\nLista de contactos después de agregar:");
-imprimirContactos();
-
-console.log("\nBorrando contacto...");
-borrarContacto("Maria Lopez");
-
-console.log("\nLista de contactos después de borrar:");
 imprimirContactos();
 
 
+
+/*
+Ajusta las funciones de crear y eliminar un contacto para que puedan almacenar la siguiente información como objetos:
+id
+nombres
+apellidos
+teléfono
+ubicaciones
+ciudad
+dirección*/
